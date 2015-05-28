8dc07953888f52d0590a55db2834a96cc484f793
Kenneth Pierce <knyrii@gmail.com>
2015-05-28 11:02:59 -0400

    Functions I always end up adding on larger projects.

---
+++ Libraries/src/simple/util/NumberUtil.java
@@ -0,0 +1,112 @@
+/**
+ *
+ */
+package simple.util;
+
+/**
+ * Could be smaller, but trying to keep the up-converts to a minimum.
+ * @author Kenneth Pierce
+ */
+public final class NumberUtil{
+	private NumberUtil(){}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(long n, long s, long e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(int n, int s, int e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(short n, short s, short e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(byte n, byte s, byte e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(int n, short s, short e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(int n, byte s, byte e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(short n, int s, int e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(byte n, int s, int e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(double n, double s, double e){
+		return n > s && n < e;
+	}
+	/**
+	 * Convenience for n > s && n < e
+	 * @param n number being compared
+	 * @param s start
+	 * @param e end
+	 * @return
+	 */
+	public static boolean between(float n, float s, float e){
+		return n > s && n < e;
+	}
+}
