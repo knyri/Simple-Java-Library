3e6f482161e3fb48303bb4c06eea3134828ca9c9
Kenneth Pierce <knyrii@gmail.com>
2015-11-09 14:43:35 -0500

    Removed the caches and made them case insensitive

--- Libraries/src/simple/io/FilterFactory.java
+++ Libraries/src/simple/io/FilterFactory.java
@@ -5,7 +5,8 @@ package simple.io;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.filechooser.FileFilter;
 
@@ -14,72 +15,104 @@ import javax.swing.filechooser.FileFilter;
  *
  */
 public final class FilterFactory {
-	public static File[] listFiles(String dir, String ext) {
+	public static File[] listFiles(String dir, String... ext) {
 		File file = new File(dir);
 		return file.listFiles(createFilenameFilter(ext));
 	}
-	static HashMap<String,FileFilter> ffcache = new HashMap<String,FileFilter>();
-	static HashMap<String,FilenameFilter>fnfcache = new HashMap<String,FilenameFilter>();
-	public static FilenameFilter createFilenameFilter(String list) {
-		final String key = list.toLowerCase();
-		FilenameFilter ff = fnfcache.get(key);
-		if (ff==null)
-			ff = new FilenameFilter() {
-					final String flist = key;
-					@Override
-					public boolean accept(File dir,String file) {
-						String ext = file.substring(file.lastIndexOf('.')+1);
-						return flist.contains(ext);
-					}
-				};
-			else
-				fnfcache.put(key, ff);
-		return ff;
-	}
-	public static FileFilter createFileFilter(String list) {
-		final String key = list.toLowerCase();
-		FileFilter ff = ffcache.get(key);
-		if (ff==null)
-			ff = new FileFilter() {
-					final String flist = key;
-					@Override
-					public boolean accept(File file) {
-						String ext = file.toString();
-						ext = ext.substring(ext.lastIndexOf('.')+1);
-						return flist.contains(ext);
-					}
-
-					@Override
-					public String getDescription() {
-						return flist;
-					}
-				};
-			else
-				ffcache.put(key, ff);
-		return ff;
-	}
-	public static FileFilter createFileFilter(final String ext, final String desc) {
-		return new javax.swing.filechooser.FileFilter() {
-			final String extention = ext.intern();
-			final String description = desc.intern();
-			@Override
-			public boolean accept(File file) {return file.getName().endsWith(extention) || file.isDirectory();}
-			@Override
-			public String getDescription() {return description;}
-		};
-	}
-	public static FilenameFilter createFilenameFilter(final File dir, final String ext) {
+	/**
+	 * Case insensitive extension filename filter.
+	 * @param list List of wanted extensions
+	 * @return
+	 */
+	public static FilenameFilter createFilenameFilter(String... list) {
+		final List<String> exts= new LinkedList<>();
+		for(String ext: list){
+			exts.add(ext.toLowerCase());
+		}
 		return new FilenameFilter() {
-			final File directory = dir;
-			final String extention = ext;
 			@Override
-			public boolean accept(File dir, String name) {
-				if (directory != null) {
-					if (dir.equals(directory))
-						if (name.endsWith(extention)) return true;
-				} else if (name.endsWith(extention)) return true;
+			public boolean accept(File dir,String file) {
+				String ext = file.substring(file.lastIndexOf('.')+1).toLowerCase();
+				for(String s: exts){
+					if(s.equals(ext)){
+						return true;
+					}
+				}
 				return false;
 			}
 		};
+	}
+	/**
+	 * Case insensitive file filter. Creates the description from
+	 * the list of extensions.
+	 * @param list List of wanted extensions.
+	 * @return
+	 */
+	public static FileFilter createFileFilter(String... list) {
+		final List<String> exts= new LinkedList<>();
+		StringBuilder descb= new StringBuilder(list.length*5);
+		for(String ext: list){
+			exts.add(ext.toLowerCase());
+			descb.append(ext).append(',');
+		}
+		descb.deleteCharAt(descb.length()-1);
+		final String desc= descb.toString();
+		return new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				String ext= file.getName();
+				ext= ext.substring(ext.lastIndexOf('.')+1).toLowerCase();
+				for(String s: exts){
+					if(s.equals(ext)){
+						return true;
+					}
+				}
+				return false;
+			}
+
+			@Override
+			public String getDescription() {
+				return desc;
+			}
+		};
+	}
+	/**
+	 * Case insensitive extension filter.
+	 * @param ext
+	 * @param desc
+	 * @return
+	 */
+	public static FileFilter createFileFilter(final String ext, final String desc) {
+		final String extl= ext.toLowerCase();
+		return new javax.swing.filechooser.FileFilter() {
+			@Override
+			public boolean accept(File file) {return file.isDirectory() || file.getName().toLowerCase().endsWith(extl);}
+			@Override
+			public String getDescription() {return desc;}
+		};
+	}
+	/**
+	 * case insensitive extension filter
+	 * @param directory nullable
+	 * @param ext
+	 * @return
+	 */
+	public static FilenameFilter createFilenameFilter(final File directory, final String ext) {
+		final String extl= ext.toLowerCase();
+		if(directory != null){
+			return new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return directory.equals(dir) && name.toLowerCase().endsWith(extl);
+				}
+			};
+		} else {
+			return new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					return name.toLowerCase().endsWith(extl);
+				}
+			};
+		}
 	}
 }
