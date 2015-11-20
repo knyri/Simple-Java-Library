84c8b60f7e7337c424c4fbb93e4955364d4959f4
Kenneth Pierce <knyrii@gmail.com>
2015-11-20 17:50:13 -0500

    Missed file.

---
+++ Libraries/src/simple/util/ImmutableList.java
@@ -0,0 +1,141 @@
+package simple.util;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * This class is just a wrapper. Any updated to the original will be reflected in this class.
+ * This class is not thread safe.
+ * All add, set, and remove operations throw UnsupportedOperationException
+ *
+ * @author Kenneth Pierce
+ *
+ * @param <E>
+ */
+public class ImmutableList<E> implements List<E> {
+	private final List<E> list;
+	/**
+	 * @param list The list backing this list
+	 */
+	public ImmutableList(List<E> list){
+		this.list= list;
+	}
+
+	@Override
+	public boolean add(E arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void add(int arg0, E arg1) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends E> arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(int arg0, Collection<? extends E> arg1) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public E remove(int arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public E set(int index, E element) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return list.contains(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return list.containsAll(c);
+	}
+
+	@Override
+	public E get(int index) {
+		return list.get(index);
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		return list.indexOf(o);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return list.iterator();
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		return list.lastIndexOf(o);
+	}
+
+	@Override
+	public ListIterator<E> listIterator() {
+		return list.listIterator();
+	}
+
+	@Override
+	public ListIterator<E> listIterator(int index) {
+		return list.listIterator(index);
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+
+	@Override
+	public List<E> subList(int fromIndex, int toIndex) {
+		return list.subList(fromIndex, toIndex);
+	}
+
+	@Override
+	public Object[] toArray() {
+		return list.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return list.toArray(a);
+	}
+
+}
