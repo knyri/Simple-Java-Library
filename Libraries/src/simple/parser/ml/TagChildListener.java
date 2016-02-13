8f031551cdf8f3fe4003db83e648ff7a74ae5fa5
Knyri <knyrii@gmail.com>
2016-02-13 12:55:43 -0500

    Comments

--- Libraries/src/simple/parser/ml/TagChildListener.java
+++ Libraries/src/simple/parser/ml/TagChildListener.java
@@ -1,5 +1,9 @@
 package simple.parser.ml;
 
+/**
+ * Listens for changes in the children.
+ * @author Ken Pierce
+ */
 public interface TagChildListener{
 	public void childTagAdded(Tag parent, Tag child);
 	public void childTagRemoved(Tag parent, Tag child);
--- Libraries/src/simple/parser/ml/TagParentListener.java
+++ Libraries/src/simple/parser/ml/TagParentListener.java
@@ -1,5 +1,9 @@
 package simple.parser.ml;
 
+/**
+ * Listens for changes in the parent tag
+ * @author Ken Pierce
+ */
 public interface TagParentListener{
 	public void newParentTag(Tag child, Tag oldP, Tag newP);
 }
