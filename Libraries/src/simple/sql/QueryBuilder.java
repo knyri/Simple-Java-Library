4135d60521006ce18101083b4054b17dd1ca3cf6
Knyri <knyri@github>
2018-02-06 13:55:23 -0500

    Query Builder and extra functions on Where Builder

---
+++ Libraries/src/simple/sql/QueryBuilder.java
@@ -0,0 +1,214 @@
+package simple.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class QueryBuilder {
+	private final String baseTable;
+	private final WhereBuilder where= new WhereBuilder();
+	private int limit= -1, offset= -1;
+	private String orderBy= "";
+	private final List<String> fields= new LinkedList<String>();
+	private final List<String> groupBy= new LinkedList<String>();
+	private final List<TableJoin> joins= new LinkedList<TableJoin>();
+
+	/**
+	 * Run parameters for the final query
+	 */
+	private Map<String, Object> runArgs;
+	/**
+	 * The final query
+	 */
+	private String query;
+
+	public QueryBuilder(String baseTable){
+		this.baseTable= baseTable;
+	}
+
+	public QueryBuilder setLimit(int limit){
+		this.limit= limit;
+		return this;
+	}
+	public QueryBuilder setOffset(int offset){
+		this.offset= offset;
+		return this;
+	}
+	public QueryBuilder orderBy(String field, boolean ascending){
+		orderBy += "," + field + " " + (ascending ? "ASC" : "DESC");
+		return this;
+	}
+	public QueryBuilder groupBy(String... fields){
+		for(String field: fields){
+			groupBy.add(field);
+		}
+		return this;
+	}
+	public QueryBuilder addField(String field){
+		fields.add(field);
+		return this;
+	}
+	public QueryBuilder leftJoin(String table, String on){
+		joins.add(new TableJoin("left", table, on));
+		return this;
+	}
+	public QueryBuilder leftJoin(String table, WhereBuilder on){
+		joins.add(new TableJoin("left", table, on));
+		return this;
+	}
+	public QueryBuilder rightJoin(String table, String on){
+		joins.add(new TableJoin("right", table, on));
+		return this;
+	}
+	public QueryBuilder rightJoin(String table, WhereBuilder on){
+		joins.add(new TableJoin("right", table, on));
+		return this;
+	}
+	public QueryBuilder join(String table, String on){
+		joins.add(new TableJoin("", table, on));
+		return this;
+	}
+	public QueryBuilder join(String table, WhereBuilder on){
+		joins.add(new TableJoin("", table, on));
+		return this;
+	}
+	public QueryBuilder innerJoin(String table, String on){
+		joins.add(new TableJoin("inner", table, on));
+		return this;
+	}
+	public QueryBuilder innerJoin(String table, WhereBuilder on){
+		joins.add(new TableJoin("inner", table, on));
+		return this;
+	}
+	public QueryBuilder outerJoin(String table, String on){
+		joins.add(new TableJoin("outer", table, on));
+		return this;
+	}
+	public QueryBuilder outerJoin(String table, WhereBuilder on){
+		joins.add(new TableJoin("outer", table, on));
+		return this;
+	}
+	public WhereBuilder getWhereBuilder(){
+		return where;
+	}
+	public QueryBuilder build(){
+		runArgs= new HashMap<String, Object>();
+		runArgs.putAll(where.getValues());
+		StringBuilder query= new StringBuilder("SELECT ");
+		for(String field: fields){
+			query.append(field).append(',');
+		}
+		query.setLength(query.length() - 1);
+
+		query
+			.append(" FROM ")
+			.append(baseTable)
+		;
+
+		for(TableJoin join: joins){
+			query
+				.append(' ')
+				.append(join.getType())
+				.append(" JOIN ")
+				.append(join.getTable())
+			;
+			if(!join.getOn().isEmpty()){
+				query
+					.append(" ON (")
+					.append(join.getOn())
+					.append(')')
+				;
+				runArgs.putAll(join.getOnValues());
+			}
+		}
+
+		if(!where.getWhere().isEmpty()){
+			query
+				.append(" WHERE ")
+				.append(where.getWhere())
+			;
+		}
+
+		if(!groupBy.isEmpty()){
+			query.append(" GROUP BY ");
+			for(String field: groupBy){
+				query
+					.append(field)
+					.append(',')
+				;
+			}
+			query.setLength(query.length() - 1);
+		}
+
+		if(!orderBy.isEmpty()){
+			query
+				.append(" ORDER BY ")
+				.append(orderBy)
+			;
+		}
+
+		if(limit > 0){
+			query
+				.append(" LIMIT ")
+				.append(Integer.toString(limit, 10))
+			;
+		}
+
+		if(offset > 0){
+			query
+				.append(" OFFSET ")
+				.append(Integer.toString(offset, 10))
+			;
+		}
+
+		this.query= query.toString();
+		return this;
+	}
+	public String getQuery(){
+		return query;
+	}
+	public Map<String, Object> getParams(){
+		return runArgs;
+	}
+	public NamedParamStatement prepare(Connection con) throws SQLException{
+		NamedParamStatement stm= new NamedParamStatement(con, query);
+		stm.setAll(runArgs);
+		return stm;
+	}
+	private final static class TableJoin{
+		private final String
+			type,
+			table,
+			on;
+		private final WhereBuilder onWb;
+		public TableJoin(String type, String table, String on){
+			this.type= type;
+			this.table= table;
+			this.on= (on == null ? "" : on);
+			onWb= null;
+		}
+		public TableJoin(String type, String table, WhereBuilder on){
+			this.type= type;
+			this.table= table;
+			this.on= null;
+			onWb= on;
+		}
+		public String getType(){
+			return type;
+		}
+		public String getTable(){
+			return table;
+		}
+		public String getOn(){
+			return on == null ? onWb.getWhere() : on;
+		}
+		@SuppressWarnings("unchecked")
+		public Map<String, Object> getOnValues(){
+			return onWb == null ? Collections.EMPTY_MAP : onWb.getValues();
+		}
+	}
+}
--- Libraries/src/simple/sql/WhereBuilder.java
+++ Libraries/src/simple/sql/WhereBuilder.java
@@ -2,30 +2,36 @@ package simple.sql;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * For use with {@link NamedParamStatement}
+ */
 public class WhereBuilder {
-	private static volatile long instanceCnt= 0;
-	private static Object instanceLock= new Object();
+	private static volatile AtomicLong instanceCnt= new AtomicLong();
 	private final String prefix;
 	private int paramIdx= 0;
 	private final Map<String, Object> values= new HashMap<String, Object>();
 	private final StringBuilder where= new StringBuilder();
 	public WhereBuilder(){
-		synchronized(instanceLock){
-			// Attempt to be thread safe
-			prefix= ":WhereBuilder" + Long.toString(instanceCnt, 10);
-			if(++instanceCnt == Long.MAX_VALUE){
-				instanceCnt= 0;
-			}
-		}
+		prefix= ":WhereBuilder" + instanceCnt.getAndIncrement();
 	}
+	/**
+	 * The where clause without the WHERE keyword
+	 * @return
+	 */
 	public final String getWhere(){
 		return where.toString();
 	}
+	/**
+	 * All the values supplied
+	 * @return A map of the placeholders and values
+	 */
 	public final Map<String, Object> getValues(){
 		return values;
 	}
 	/**
+	 * Creates a named placeholder for the value
 	 * @param v
 	 * @return A placeholder for the added value
 	 */
@@ -34,58 +40,137 @@ public class WhereBuilder {
 		values.put(ref, v);
 		return ref;
 	}
+	/**
+	 * Appends ' AND '
+	 * @return
+	 */
 	public final WhereBuilder and(){
 		if(paramIdx > 0){
 			where.append(" AND ");
 		}
 		return this;
 	}
+	/**
+	 * Appends ' OR '
+	 * @return
+	 */
 	public final WhereBuilder or(){
 		if(paramIdx > 0){
 			where.append(" OR ");
 		}
 		return this;
 	}
+	/**
+	 * AND (wb)
+	 * Further changes to wb will not be applied.
+	 * @param wb
+	 * @return
+	 */
 	public final WhereBuilder and(WhereBuilder wb){
 		and();
 		where.append('(').append(wb).append(')');
 		values.putAll(wb.values);
 		return this;
 	}
+	/**
+	 * OR (wb)
+	 * Further changes to wb will not by applied.
+	 * @param wb
+	 * @return
+	 */
 	public final WhereBuilder or(WhereBuilder wb){
 		or();
-		where.append('(').append(wb).append(')');
+		where.append('(').append(wb.getWhere()).append(')');
 		values.putAll(wb.values);
 		return this;
 	}
+	/**
+	 * expr IS NULL
+	 * @param expr
+	 * @return
+	 */
 	public WhereBuilder isNull(String expr){
 		where
 			.append(expr)
 			.append(" IS NULL");
 		return this;
 	}
+	/**
+	 * expr IS NOT NULL
+	 * @param expr
+	 * @return
+	 */
 	public WhereBuilder isNotNull(String expr){
 		where
 			.append(expr)
 			.append(" IS NOT NULL");
 		return this;
 	}
-	public WhereBuilder like(String expr, String like){
+	/**
+	 * expr LIKE pattern
+	 * @param expr
+	 * @param pattern
+	 * @return
+	 */
+	public WhereBuilder like(String expr, String pattern){
 		where
 			.append(expr)
 			.append(" LIKE ")
-			.append(addValue(like));
+			.append(addValue(pattern));
 		return this;
 	}
+	/**
+	 * expr NOT LIKE pattern
+	 * @param expr
+	 * @param pattern
+	 * @return
+	 */
+	public WhereBuilder notLike(String expr, String pattern){
+		where
+			.append(expr)
+			.append(" NOT LIKE ")
+			.append(addValue(pattern));
+		return this;
+	}
+	/**
+	 * expr BETWEEN start AND end
+	 * @param expr
+	 * @param start
+	 * @param end
+	 * @return
+	 */
 	public WhereBuilder between(String expr, Object start, Object end){
 		where
 			.append(expr)
-			.append("BETWEEN ")
+			.append(" BETWEEN ")
 			.append(addValue(start))
 			.append(" AND ")
 			.append(addValue(end));
 		return this;
 	}
+	/**
+	 * expr NOT BETWEEN start AND end
+	 * @param expr
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	public WhereBuilder notBetween(String expr, Object start, Object end){
+		where
+			.append(expr)
+			.append(" NOT BETWEEN ")
+			.append(addValue(start))
+			.append(" AND ")
+			.append(addValue(end));
+		return this;
+	}
+	/**
+	 * expr op value
+	 * @param expr
+	 * @param op
+	 * @param value
+	 * @return
+	 */
 	public WhereBuilder where(String expr, String op, Object value){
 		where
 			.append(expr)
@@ -95,6 +180,104 @@ public class WhereBuilder {
 			.append(addValue(value));
 		return this;
 	}
+	/**
+	 * expr < value
+	 * @param expr
+	 * @param value
+	 * @return
+	 */
+	public WhereBuilder lessThan(String expr, Object value){
+		where
+			.append(expr)
+			.append(" < ")
+			.append(addValue(value));
+		return this;
+	}
+	/**
+	 * expr = value
+	 * @param expr
+	 * @param value
+	 * @return
+	 */
+	public WhereBuilder equal(String expr, Object value){
+		where
+			.append(expr)
+			.append(" = ")
+			.append(addValue(value));
+		return this;
+	}
+	/**
+	 * expr > value
+	 * @param expr
+	 * @param value
+	 * @return
+	 */
+	public WhereBuilder greaterThan(String expr, Object value){
+		where
+			.append(expr)
+			.append(" > ")
+			.append(addValue(value));
+		return this;
+	}
+	/**
+	 * expr != value
+	 * @param expr
+	 * @param value
+	 * @return
+	 */
+	public WhereBuilder notEqual(String expr, Object value){
+		where
+			.append(expr)
+			.append(" != ")
+			.append(addValue(value));
+		return this;
+	}
+	/**
+	 * expr <= value
+	 * @param expr
+	 * @param value
+	 * @return
+	 */
+	public WhereBuilder lessThanOrEqual(String expr, Object value){
+		where
+			.append(expr)
+			.append(" <= ")
+			.append(addValue(value));
+		return this;
+	}
+	/**
+	 * expr >= value
+	 * @param expr
+	 * @param value
+	 * @return
+	 */
+	public WhereBuilder greaterThanOrEqual(String expr, Object value){
+		where
+			.append(expr)
+			.append(" >= ")
+			.append(addValue(value));
+		return this;
+	}
+	/** Adds a '('
+	 * @return
+	 */
+	public WhereBuilder startGroup(){
+		where.append('(');
+		return this;
+	}
+	/** Adds a ')'
+	 * @return
+	 */
+	public WhereBuilder endGroup(){
+		where.append(')');
+		return this;
+	}
+	/**
+	 * expr IN (values[0], values[1], ...)
+	 * @param expr
+	 * @param values
+	 * @return
+	 */
 	public WhereBuilder in(String expr, Object... values){
 		where
 			.append(expr)
@@ -103,6 +286,31 @@ public class WhereBuilder {
 			where.append(addValue(value)).append(',');
 		}
 		where.setCharAt(where.length() - 1, ')');
+		return this;
+	}
+	/**
+	 * expr NOT IN (...)
+	 * @param expr
+	 * @param values
+	 * @return
+	 */
+	public WhereBuilder notIn(String expr, Object... values){
+		where
+			.append(expr)
+			.append(" NOT IN (");
+		for(Object value: values){
+			where.append(addValue(value)).append(',');
+		}
+		where.setCharAt(where.length() - 1, ')');
+		return this;
+	}
+	/**
+	 * Adds the literal to the where clause
+	 * @param literal
+	 * @return
+	 */
+	public WhereBuilder literal(String literal){
+		where.append(literal);
 		return this;
 	}
 }
