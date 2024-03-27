91cf8116b2675c1cee1205e24b0aef4c8b130a9c
knyri <knyri@github>
2024-03-27 10:50:36 -0400

    Paths no longer parsed as flags

--- Libraries/src/simple/util/command/CLIParameters.java
+++ Libraries/src/simple/util/command/CLIParameters.java
@@ -1,7 +1,10 @@
 package simple.util.command;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import simple.io.ParseException;
 
@@ -15,7 +18,7 @@ import simple.io.ParseException;
  * --param=value
  *    Sets the value
  * --param
- *    Sets the value to 1 (boolean parameter)
+ *    Sets the value to 1 (boolean parameter/switch)
  * --param=
  *    unsets the parameter
  *
@@ -34,14 +37,27 @@ import simple.io.ParseException;
  *
  */
 public class CLIParameters {
-	private final HashMap<String, String> params= new HashMap<String, String>();
-	private final HashSet<Character> flags= new HashSet<Character>();
+	// An unmodifiable version are set to these
+	private final Map<String, String> params;
+	private final Set<Character> flags;
+	private final Set<String> bigFlags;
 	private final boolean caseSensitive;
 	private final String[] args;
-	private int argsParsed;
+	private int argsParsed= 0;
+	public Set<String> getStringFlags(){
+		return bigFlags;
+	}
+	public Set<Character> getFlags(){
+		return flags;
+	}
+	public Map<String, String> getParams(){
+		return params;
+	}
 	public CLIParameters(String[] args, boolean caseSensitiveFlags) throws ParseException{
 		caseSensitive= caseSensitiveFlags;
-		argsParsed= args.length;
+		HashMap<String, String> params= new HashMap<String, String>();
+		HashSet<Character> flags= new HashSet<Character>();
+		HashSet<String> bigFlags= new HashSet<>();
 		out:
 		for(int i= 0, len= args.length; i < len; i++){
 			if(args[i].length() == 0 || args[i].equals("-")){
@@ -49,6 +65,11 @@ public class CLIParameters {
 			}
 			switch(args[i].charAt(0)){
 			case '/':
+				if(args[i].lastIndexOf('/') != 0) {
+					argsParsed= i;
+					// it's a path
+					break out;
+				}
 				// flags
 				// e.g. /fjk will set flags f, j, and k
 				// /-fjk will unset flags f, j, and k
@@ -78,12 +99,18 @@ public class CLIParameters {
 				if(args[i].charAt(1) == '-'){
 					// --key=value
 					if(args[i].length() == 2){
+						// "--" The stop processing indicator
 						argsParsed= i + 1;
 						break out;
 					}
 					String[] kv= args[i].split("=", 2);
 					if(kv.length == 1){
-						params.remove(kv[0].substring(2));
+						if(kv[0].endsWith("=")) {
+							params.remove(kv[0].substring(2));
+							bigFlags.remove(caseSensitive ? kv[0].substring(2) : kv[0].substring(2).toLowerCase());
+						}else{
+							bigFlags.add(caseSensitive ? kv[0].substring(2) : kv[0].substring(2).toLowerCase());
+						}
 					}else{
 						params.put(kv[0].substring(2), kv[1]);
 					}
@@ -107,6 +134,9 @@ public class CLIParameters {
 			this.args= new String[args.length - argsParsed];
 			System.arraycopy(args, argsParsed, this.args, 0, this.args.length);
 		}
+		this.bigFlags= Collections.unmodifiableSet(bigFlags);
+		this.flags= Collections.unmodifiableSet(flags);
+		this.params= Collections.unmodifiableMap(params);
 	}
 
 	public String getArg(int i){
@@ -114,6 +144,16 @@ public class CLIParameters {
 	}
 	public int getArgCount(){
 		return args.length;
+	}
+
+	/**
+	 * Returns
+	 *
+	 * @param param
+	 * @return
+	 */
+	public boolean isSet(String param) {
+		return bigFlags.contains(caseSensitive ? param : param.toLowerCase());
 	}
 
 	/**
