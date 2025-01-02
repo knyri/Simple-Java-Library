a14861924da606af46154df35356f0fe9038ba7c
knyri <knyri@github>
2025-01-02 13:52:50 -0500

    *Element() functions return Element

--- Libraries/src/simple/w3c/NodeUtil.java
+++ Libraries/src/simple/w3c/NodeUtil.java
@@ -1,5 +1,6 @@
 package simple.w3c;
 
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -7,10 +8,10 @@ import org.w3c.dom.NodeList;
 public final class NodeUtil{
 	private NodeUtil(){}
 
-	public static Node firstElement(NodeList list){
+	public static Element firstElement(NodeList list){
 		Node tmp= list.item(0);
 		if(tmp.getNodeType() == Node.ELEMENT_NODE){
-			return tmp;
+			return (Element)tmp;
 		}
 		for(int idx= 1, end= list.getLength(); idx< end; idx++){
 			tmp= list.item(idx);
@@ -18,26 +19,45 @@ public final class NodeUtil{
 				break;
 			}
 		}
-		return tmp;
+		return (Element)tmp;
 	}
-	public static Node nextElement(Node from){
+	public static Element nextElement(Node from){
 		while((null != (from= from.getNextSibling())) && from.getNodeType() != Node.ELEMENT_NODE);
-		return from;
+		return (Element)from;
 	}
-	public static Node previousElement(Node from){
+	public static Element previousElement(Node from){
 		while((null != (from= from.getPreviousSibling())) && from.getNodeType() != Node.ELEMENT_NODE);
-		return from;
+		return (Element)from;
 	}
-	public static Node firstChildElement(Node parent){
+	public static Element firstChildElement(Node parent){
 		Node tmp= parent.getFirstChild();
 		if(tmp.getNodeType() != Node.ELEMENT_NODE){
 			tmp= nextElement(tmp);
 		}
-		return tmp;
+		return (Element)tmp;
 	}
-	public static Node findElement(Node start, String name){
-		Node tmp= nextElement(start);
-		while(tmp != null && !name.equals(tmp.getLocalName())){
+	/**
+	 * Finds the element named [name]. Considers the [start] node
+	 * @param start
+	 * @param name
+	 * @return
+	 */
+	public static Element findElement(Node start, String name){
+		Node tmp= start;
+		while(tmp != null && !name.equals(tmp.getNodeName())){
+			tmp= nextElement(tmp);
+		}
+		return (Element)tmp;
+	}
+	/**
+	 * Finds the next element named [name]. Does not consider the [start] node
+	 * @param start
+	 * @param name
+	 * @return
+	 */
+	public static Element findNextElement(Node start, String name){
+		Element tmp= nextElement(start);
+		while(tmp != null && !name.equals(tmp.getNodeName())){
 			tmp= nextElement(tmp);
 		}
 		return tmp;
